971520d01aa7f8cc5dfb1686cc00c4f5690b11a4
abane1 <abanesebiane@outlook.fr>
2022-04-12 12:09:09 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -1,21 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let link = document.querySelector("main p a");
+    let link = document.querySelector("main p");
     let btn1 = document.querySelector("main nav a:first-child");
     let btn2 = document.querySelector("main nav a:nth-child(2)");
-    console.log(btn1)
+    console.log(link)
     console.log(btn2)
     
-    link.addEventListener("click", () => {
-        if (btn1.innerHTML === `<a href="">Recherche Google</a>` && btn2.innerHTML === `<a href="">J'ai de la chance</a>`){
-
+    link.addEventListener("onclick", () => {
+/*         if (btn1.innerHTML == `<a href="">Recherche Google</a>` && btn2.innerHTML == `<a href="">J'ai de la chance</a>`){
             btn1.innerHTML = `<a href="">Google Search</a>`;
             btn2.innerHTML = `<a href="">I'm lucky</a>`;
         }
         else{
             btn1.innerHTML = `<a href="">Recherche Google</a>`;
             btn2.innerHTML = `<a href="">J'ai de la chance</a>`;
-        }
-        link.innerHTML = `<p>Google in : <a href="">French</a></p>`;
-        console.log('boijhfds')
+            btn1.innerHTML = `<a href="">Google Search</a>`;
+            btn2.innerHTML = `<a href="">I'm lucky</a>`;
+        } */
+        btn1.innerHTML = `<a href="">Google Search</a>`;
+        btn2.innerHTML = `<a href="">I'm lucky</a>`;
+        
     });
 });
